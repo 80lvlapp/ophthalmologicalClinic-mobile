e63ebd7eda7beefa3f274f2e08bcff9826fb3f05
inzarubin80@gmail.com <inzarubin80@gmail.com>
2022-12-12 23:06:52 +0300

    +

---
+++ src/screens/MedicalDocument.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-elements';
+
+export const MedicalDocument = () => {
+
+    return (
+        <View style={{ flex: 1, backgroundColor: "white", padding: 2 }}>
+            <Text>MedicalDocument </Text>
+        </View>
+
+    )
+
+}
