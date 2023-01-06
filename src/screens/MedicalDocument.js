6f386032aa2798facfbf0a3aba951c91ac83c6b6
inzarubin80@gmail.com <inzarubin80@gmail.com>
2023-01-06 14:26:01 +0300

    [000000017] Сделать карусель разделов...

--- src/screens/MedicalDocument.js
+++ src/screens/MedicalDocument.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { ButtonGroup } from '@rneui/themed'
+import { View, StyleSheet, ScrollView, TouchableOpacity, Text} from 'react-native';
 import DataTable from './../components/DataTable'
 import {getTemplate} from './../templates';
+import { THEME } from './../themes'
 
 export const MedicalDocument = ({navigation, route}) => {
     
@@ -29,16 +29,18 @@ export const MedicalDocument = ({navigation, route}) => {
 
     return (
         <View style={{ flex: 1, backgroundColor: "white", padding: 2 }}>
+            <View>
+				<ScrollView horizontal={true} style={[styles.contentContainer]} >
+					{template.sections.map((item, index) =>
+						<View style={styles.wraperSectionsItem} key={item.id} >
+							<TouchableOpacity style={[styles.sectionItem, index == selectedIndex ? styles.selectSectionItem : null]} onPress={() =>  setSelectedIndex(index)}>
+								<Text numberOfLines={2} style={[styles.doctorItemText, index == selectedIndex ? styles.selectSectionItem : null]}>{item.name}</Text>
+							</TouchableOpacity>
+						</View>)}
+				</ScrollView>
+			</View>
 
-            <ButtonGroup
-                buttons={template.sections.map(item=>item.name)}
-                selectedIndex={selectedIndex}
-                onPress={(value) => {
-                    setSelectedIndex(value);
-                }}
-                containerStyle={{ marginBottom: 20 }}
-            />
-
+       
            <DataTable tableRowsData={tableRowsData} onChangeTextCell={onChangeTextCell} tableValue={tableValue}/>
            
         </View>
@@ -48,5 +50,38 @@ export const MedicalDocument = ({navigation, route}) => {
 
 
 const styles = StyleSheet.create({
+    contentContainer: {
+        padding: 10,
+    },
 
+    wraperSectionsItem: {
+        padding: 5,
+    },
+
+    sectionItem: {
+        borderWidth: 1,
+        width: 170,
+        height: 60,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "white",
+        padding: 4
+    },
+
+
+    selectSectionItem: {
+        backgroundColor: THEME.SELECT_COLOR,
+        color: 'white',
+        borderWidth: 0
+    },
+    doctorItemText: {
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        color: 'black',
+        fontSize: 18,
+        width: "100%",
+        height: "100%",
+
+    },
 })
